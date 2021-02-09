3a0fd235da94864bf51b321e723475ee23cd9635
pjauth <43118225+pjauth@users.noreply.github.com>
2021-02-09 12:47:08 -0500

    Add files via upload

---
+++ misc.c
@@ -0,0 +1,39 @@
+/* misc.c */
+
+// #include "memwatch.h"
+#include <stdlib.h>
+
+
+static char *cr = "copyright (c) David Binkley 2021";
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int abort1(char *s)
+{
+    fprintf(stderr, "%s\n", s);
+    exit(-1);
+}
+
+
+void *check_malloc(int size)
+{
+    char *cp;
+    /*printf("check malloc allocating %d bytes\n", size) ;*/
+
+    cp = malloc(size) ;
+    if (cp == NULL)
+        abort1("check_malloc: No Core") ;
+
+    return (cp) ;
+}
+
+
+void *check_realloc(void *p, int size)
+{
+    p = realloc(p, size);
+    if (p == NULL)
+        abort1("check_realloc: No Core") ;
+    return(p);
+}
+
